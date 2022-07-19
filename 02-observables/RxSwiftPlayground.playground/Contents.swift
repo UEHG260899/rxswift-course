c2bbec1d29d4c7e1e6be565c482968c43b110390
Uriel Enrique <urielenrique99@gmail.com>
2022-07-18 23:18:57 -0500

    Observable playground done

--- 02-observables/RxSwiftPlayground.playground/Contents.swift
+++ 02-observables/RxSwiftPlayground.playground/Contents.swift
@@ -34,3 +34,199 @@ import RxSwift
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
+
+example(of: "just, of, from") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    // Makes an observable sequence of just one
+    let observable = Observable<Int>.just(one)
+    // "                          " of the values provided
+    let observable2 = Observable.of(one, two, three)
+    // "                          " of int arrays
+    let observable3 = Observable.of([one, two, three])
+    // "                          " of the element types contained in the collection
+    let observable4 = Observable.from([one, two, three])
+}
+
+
+example(of: "subscribe") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    let observable = Observable.of(one, two, three)
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    })
+}
+
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    // Doesn´t emmit but completes
+    observable.subscribe(
+        onNext: { element in
+            print(element)
+        },
+        onCompleted: {
+            print("Completed")
+        }
+    )
+}
+
+example(of: "never") {
+    let observable = Observable<Void>.never()
+    
+    // Doesn´t emmit and never completes
+    observable
+        .debug("Debug: ", trimOutput: false)
+        .subscribe(
+            onNext: { element in
+
+            },
+            onCompleted: {
+                print("Completed")
+            }
+        )
+    
+}
+
+example(of: "range") {
+    let observable = Observable<Int>.range(start: 1, count: 10)
+    
+    observable
+        .subscribe(
+            onNext: { i in
+                
+                let n = Double(i)
+                
+                let fibonacci = Int(
+                    ((pow(1.61803, n) - pow(0.61803, n)) /
+                     2.23606).rounded()
+                )
+                
+                print(fibonacci)
+            }
+        )
+}
+
+example(of: "dispose") {
+    
+    let observable = Observable.of("A", "B", "C")
+    
+    let subscription = observable.subscribe { event in
+        print(event)
+    }
+    
+    subscription.dispose()
+    
+}
+
+example(of: "DisposeBag") {
+    
+    let dispodeBag = DisposeBag()
+    
+    Observable.of("A", "B", "C")
+        .subscribe {
+            print($0)
+        }
+        .disposed(by: dispodeBag)
+}
+
+example(of: "create") {
+    
+    enum MyError: Error {
+        case anError
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    Observable<String>.create { observer in
+        observer.onNext("1")
+        
+        observer.onError(MyError.anError)
+        
+        observer.onCompleted()
+        
+        observer.onNext("?")
+        
+        return Disposables.create()
+    }
+    .subscribe(onNext: {print($0)},
+               onError: {print($0)},
+               onCompleted: {print("Completed")},
+               onDisposed: {print("Disposed")})
+    .disposed(by: disposeBag)
+}
+
+example(of: "deferred") {
+    let disposeBag = DisposeBag()
+    
+    var flip = false
+    
+    let factory: Observable<Int> = Observable.deferred({
+        flip.toggle()
+        
+        if flip {
+            return Observable.of(1,2,3)
+        } else  {
+            return Observable.of(4,5,6)
+        }
+    })
+    
+    for _ in 0...3 {
+        factory.subscribe(onNext: {
+            print($0, terminator: "")
+        })
+        .disposed(by: disposeBag)
+        
+        print()
+    }
+}
+
+example(of: "Single") {
+    let disposeBag = DisposeBag()
+    
+    enum FileReaderError: Error {
+        case fileNotFound, unreadable, encodingFailed
+    }
+    
+    func loadText(from name: String) -> Single<String> {
+        return Single.create { single in
+            
+            let disposable = Disposables.create()
+            
+            guard let path = Bundle.main.path(forResource: name, ofType: "txt") else  {
+                single(.error(FileReaderError.fileNotFound))
+                return disposable
+            }
+            
+            guard let data = FileManager.default.contents(atPath: path) else  {
+                single(.error(FileReaderError.unreadable))
+                return disposable
+            }
+            
+            guard let contents = String(data: data, encoding: .utf8) else  {
+                single(.error(FileReaderError.encodingFailed))
+                return disposable
+            }
+            
+            single(.success(contents))
+            return disposable
+        }
+    }
+    
+    loadText(from: "Copyright")
+        .subscribe {
+            switch $0 {
+            case .success(let string):
+                print(string)
+            case .error(let error):
+                print(error)
+            }
+        }
+        .disposed(by: disposeBag)
+}
