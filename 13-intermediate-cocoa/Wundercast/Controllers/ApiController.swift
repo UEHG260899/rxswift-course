4e0fd544dd908e6f19dfd053b2c1b710b4aa6ee7
Uriel Enrique <urielenrique99@gmail.com>
2022-07-25 22:59:38 -0500

    adding CLLocation Rx extension

--- 13-intermediate-cocoa/Wundercast/Controllers/ApiController.swift
+++ 13-intermediate-cocoa/Wundercast/Controllers/ApiController.swift
@@ -107,7 +107,7 @@ class ApiController {
 
   /// The api key to communicate with openweathermap.org
   /// Create you own on https://home.openweathermap.org/users/sign_up
-  private let apiKey = "<#Your Key#>"
+  private let apiKey = "1ba0f5e30f5a92d2d08f8d6d18e97bff"
 
   /// API base URL
   let baseURL = URL(string: "http://api.openweathermap.org/data/2.5")!
